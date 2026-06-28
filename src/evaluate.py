0db205c92f48ae3766f4f40bf16eadc8d460877e
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-06-28 18:30:52 +0200

    Iterate over words in text versions jointly

--- src/evaluate.py
+++ src/evaluate.py
@@ -23,8 +23,9 @@ pred_annotations = list[list[Annotation]]()
 for exp_text, pred_text in expected_corpus.zip(pred_corpus):
   if len(exp_text) == len(pred_text):
     print('Matched', exp_text.text_id)
-    expected_annotations.extend(exp_text.annotations)
-    pred_annotations.extend(pred_text.annotations)
+    for exp_word, pred_word in exp_text.zip(pred_text):
+      expected_annotations.append(exp_word.annotations)
+      pred_annotations.append(pred_word.annotations)
   else:
     print('Ignoring', exp_text.text_id)
 
