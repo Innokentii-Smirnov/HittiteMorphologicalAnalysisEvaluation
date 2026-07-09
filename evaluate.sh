c0e6361ba7efe2843b4f5cd7915a21024cd5eb7b
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-07-09 23:12:14 +0200

    Pass complete dataset name to the evaluation shell script

--- evaluate.sh
+++ evaluate.sh
@@ -1,16 +1,4 @@
-clear
-cth="$1"
-if [ -z "$cth" ]; then
-  echo 'A CTH text group should be specified as the first argument.'
-  exit
-fi
-source="$2"
-if [ -z "$source" ]; then
-  echo 'A source (HFR, TLH, etc) should be specified as the second argument.'
-  exit
-fi
-expected_dataset="CTH ${cth%-clb}_XML_$source"
-lemmatized_dataset="CTH ${cth}_XML_$source"
+dataset="$1"
 python src/evaluate.py \
-  ~/HFR/data/expected/"$expected_dataset" \
-  ~/HFR/data/lemmatized-xml/"$lemmatized_dataset"
+  ~/HFR/data/expected/"$dataset" \
+  ~/HFR/data/lemmatized-xml/"$dataset"
